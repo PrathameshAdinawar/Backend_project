3b540600eaa98da23218359c3c0bc5f08b9bc14a
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2024-10-28 12:41:26 +0530

    openning page

--- script.js
+++ script.js
@@ -15,4 +15,36 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+      nextEl: ".button-next",
+      prevEl: ".button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var acc = document.getElementsByClassName("accordion");
+
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      this.parentElement.classList.toggle("active");
+  
+      var pannel = this.nextElementSibling;
+  
+      if (pannel.style.maxHeight) {
+        pannel.style.maxHeight = null;
+        pannel.style.paddingBottom = "0"; // Collapse padding when closed
+      } else {
+        pannel.style.maxHeight = pannel.scrollHeight + "px";
+        pannel.style.paddingBottom = "2.5rem"; // Add padding when open
+      }
+    });
+  }
   
